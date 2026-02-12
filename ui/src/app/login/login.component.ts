3874af5dced37ab3491f94884313ccc2ed16af0c
LucasBxstx <lucasbeste8@gmail.com>
2026-02-12 18:31:41 +0100

    frontend: login page finished

--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../shared/components/card/card.component';
 import { CardWrapperComponent } from '../shared/components/card-wrapper/card-wrapper.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [CardComponent, CardWrapperComponent],
+  imports: [CardComponent, CardWrapperComponent, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public readonly loginFormGroup = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+      nonNullable: true,
+    }),
+  });
+
+  public login(): void {
+    if (this.loginFormGroup.invalid) {
+      return;
+    }
+  }
+}
