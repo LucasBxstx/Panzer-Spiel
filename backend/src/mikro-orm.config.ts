0a0da348d92c03ce25046cf89d4a82c2e15e85f5
LucasBxstx <lucasbeste8@gmail.com>
2026-02-12 15:47:38 +0100

    docker setup works!

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -6,32 +6,12 @@ import {
 } from '@mikro-orm/postgresql';
 import { User } from './app/api/user/user.entity'; // all docker container
 
-// all docker container
-// const user = 'finances-admin';
-// const port = 5432;
-// const password = 'myFinanceStats';
-// const dbName = 'financesDB';
-// const host = 'db';
-
+// setting for prod and development
 const user = process.env.DB_USERNAME;
 const port = parseInt(process.env.DB_PORT ?? '5432');
 const password = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
 const host = process.env.DB_HOST ?? 'db';
-
-// only db docker
-// const user = 'finances-admin';
-// const port = 5434;
-// const password = 'myFinanceStats';
-// const dbName = 'financesDB';
-// const host = '127.0.0.1';
-
-// no docker, local development
-// const user = 'postgres';
-// const port = 5432;
-// const password = 'xXLucas3Xx';
-// const dbName = 'TestPostgreSQL';
-// const host = '127.0.0.1';
 
 export default defineConfig({
   driver: PostgreSqlDriver,
