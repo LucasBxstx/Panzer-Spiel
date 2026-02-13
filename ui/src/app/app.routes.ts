13e91e31eab52140097edb403b8283cbddb9ea47
LucasBxstx <lucasbeste8@gmail.com>
2026-02-13 12:42:28 +0100

    frontend: register page and styling

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    // loadComponent: () => import('./register/register.component').then((m) => m.RegisterComponent),
+  },
+];
--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardComponent } from '../shared/components/card/card.component';
 import { CardWrapperComponent } from '../shared/components/card-wrapper/card-wrapper.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [CardComponent, CardWrapperComponent, ReactiveFormsModule],
+  imports: [CardComponent, CardWrapperComponent, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  private readonly router = inject(Router);
+
   public readonly loginFormGroup = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.required, Validators.email],
